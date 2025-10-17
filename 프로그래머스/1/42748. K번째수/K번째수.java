119c4270864ab3a8b4f8f2c75b4b63f34708f310
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-10-17 11:30:17 +0900

    [level 1] Title: K번째수, Time: 1.24 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -11,20 +11,15 @@ class Solution {
         for(int i = 0; i < commands.length; i++) {
             start = commands[i][0]; // 2 
             end = commands[i][1];
-            int[] splited = new int[end - start + 1];
             
-            int startIdx = commands[i][0] - 1;
-            for(int j = 0; j < end - start + 1; j++) {
-                splited[j] = array[startIdx];
-                startIdx = startIdx + 1;
-            }
+            int[] splitted = Arrays.copyOfRange(array, start - 1, end);
             
             // 2. 정렬
-            Arrays.sort(splited);
+            Arrays.sort(splitted);
             
             // 3. 원소 정답 배열에 추가
             target = commands[i][2] - 1;
-            answer[i] = splited[target];
+            answer[i] = splitted[target];
             
         }
         return answer;
