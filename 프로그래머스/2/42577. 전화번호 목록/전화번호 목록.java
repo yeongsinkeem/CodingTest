6c5ad0517fcf277a5dbfa3a82772c62678314c99
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-10-24 16:34:09 +0900

    [level 2] Title: 전화번호 목록, Time: 392.80 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,11 +4,12 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
-        // 정렬하면 접두사 관계인 문자열들은 반드시 인접 
+        // 1. phone_book 정렬
         Arrays.sort(phone_book);
-
+        
         for(int i = 0; i < phone_book.length - 1; i++) {
-            if(phone_book[i+1].startsWith(phone_book[i])) return false;
+            if(phone_book[i+1].startsWith(phone_book[i])) 
+                return false;
         }
         return answer;
     }
