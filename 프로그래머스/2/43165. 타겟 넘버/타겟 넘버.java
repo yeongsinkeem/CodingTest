874e98dbffeba3b52973a9b4a0cec83f947278ce
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-10-25 17:50:23 +0900

    [level 2] Title: 타겟 넘버, Time: 9.25 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,33 @@
 class Solution {
-    int answer = 0;
-    int[] numbers;
-    int target;
+    private int[] numbers;
+    public static int sum = 0;
+    
+    private int target;
+    private int count = 0;
+    private int total = 0;
     
     public int solution(int[] numbers, int target) {
+        int answer = 0;
         this.numbers = numbers;
         this.target = target;
         
-        // 1. DFS 시작 
+        // DFS(시작 노드, 현재 합)
         DFS(0, 0);
         
-        return answer;
+        return count;
     }
     
-    public void DFS(int idx, int sum) {
+    public void DFS(int node, int sum) {
         // 종료 조건 
-        if(idx == numbers.length) {
-            if(sum == target) answer++;
-            return;
-        }
+        if(node == numbers.length ) {
+            if(sum == target) {
+                count++;
+                
+            }
+        return;
+    }
         
-        DFS(idx + 1, sum + numbers[idx]);
-        DFS(idx + 1, sum - numbers[idx]);
-        
+        DFS(node + 1, sum + numbers[node]);
+        DFS(node + 1, sum - numbers[node]);
     }
 }
