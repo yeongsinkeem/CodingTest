3e001a431dd9525b91beb04e418acc4029cd7aac
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-10-25 19:19:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.92 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 55.5 MB, 시간: 9.32 ms
+메모리: 55.7 MB, 시간: 10.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 13일 00:29:56
+2025년 10월 25일 19:19:57
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,49 +1,53 @@
 import java.util.*;
 
 class Solution {
-    static int currX = 0;
-    static int currY = 0;
-
+    public static int[] dx = {0, 0, 1, -1};
+    public static int[] dy = {1, -1, 0, 0};
     
     public int solution(int[][] maps) {
+        // int answer = 1;
         int n = maps.length;
         int m = maps[0].length;
         
-        int[] xDist = {-1, 1, 0, 0};
-        int[] yDist = {0, 0, 1, -1};
+        // q = 이동할(한) 좌표 넣을 거임 
+        Queue<int[]> q = new LinkedList<>();
         
-        // 1. Queue 준비 및 시작점 추가
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0, 1});
-        maps[0][0] = 0; // 재방문 방지 
+        // 방문 좌표 : 미방문(0), 방문(++1)
+        int[][] visited = new int[n][m];
         
-        // 2. BFS 시작 
-        while(!queue.isEmpty()) {
-            // 3. 원소 빼기 
-            int[] curr = queue.poll();
+        // 1. BFS 시작 
+        q.offer( new int[]{0, 0} );
+        visited[0][0] = 1;
+        
+        while(!q.isEmpty()) {
+            int[] currPos = q.poll();
+            int currX = currPos[0];
+            int currY = currPos[1];
             
-            int x = curr[0];
-            int y = curr[1];
-            int cost = curr[2];
-            
-            // 종료 조건 
-            if( x == n-1 && y == m-1 ) {
-                return cost;
-            }
-            
-            // 4. 동서남북 탐색 
+            // 2. 동서남북 탐색 
             for(int i = 0; i < 4; i++) {
-                int newX = x + xDist[i];
-                int newY = y + yDist[i];
-                // 5. 이동 가능하다면 
-                if( newX >= 0 && newY >= 0 && newX < n && newY < m) {
-                    if( maps[newX][newY] == 1 ) {
-                        maps[newX][newY] = 0;
-                        queue.offer(new int[]{newX, newY, cost + 1});
-                    }
+                int newX = currX + dx[i];
+                int newY = currY + dy[i];
+                
+                // 3. 만약 좌표판 벗어난다면 
+                if(newX < 0 || newY < 0 || newX >= n || newY >= m) 
+                    continue;
+                
+                // 4. 만약 벽으로 막혀 있다면 
+                if(maps[newX][newY] == 0) 
+                    continue;
+                
+                // 5. 만약 미방문 좌표라면
+                if(visited[newX][newY] == 0) {
+                    // 이동할 수 있으니 큐에 좌표 넣어주기
+                    q.offer(new int[]{newX, newY});
+                    // 값 업데이트 
+                    visited[newX][newY] = visited[currX][currY] + 1;
                 }
             }
         }
-        return -1;
+        int answer = visited[n-1][m-1];
+        
+        return (answer == 0) ? -1 : answer;
     }
 }
