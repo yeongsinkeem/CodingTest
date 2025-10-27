3525b349f91660e2b98812c767d0e0b7072bfa5a
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-10-27 17:06:14 +0900

    [level 1] Title: 모의고사, Time: 1.68 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,52 +2,50 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        ArrayList<Integer> answerArr = new ArrayList<>();
-        
-        int ansLen = answers.length;
+        int n = answers.length;
         
         // 1. 시험지 생성
-        int[] test1 = new int[ansLen];
-        int[] test2 = new int[ansLen];
-        int[] test3 = new int[ansLen];
-        
-        int[] a1Arr = {1, 2, 3, 4, 5};
-        for(int i = 0; i < ansLen; i++) {
-            test1[i] = a1Arr[i % 5];
+        int[] aTest = {1, 2, 3, 4, 5};
+        int[] aAnswer = new int[n];
+        for(int i = 0; i < n; i++) {
+            aAnswer[i] = aTest[i % 5];
         }
         
-        int[] a2Arr = {2, 1, 2, 3, 2, 4, 2, 5};
-        for(int i = 0; i < ansLen; i++) {
-            test2[i] = a2Arr[i % 8];
+        int[] bTest = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] bAnswer = new int[n];
+        for(int i = 0; i < n; i++) {
+            bAnswer[i] = bTest[i % 8];
         }
         
-        int[] a3Arr = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        for(int i = 0; i < ansLen; i++) {
-            test3[i] = a3Arr[i % 10];
+        int[] cTest = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] cAnswer = new int[n];
+        for(int i = 0; i < n; i++) {
+            cAnswer[i] = cTest[i % 10];
         }
         
-        // 2. 시험지 채점 
-        int[] count = new int[3];
-        for(int i = 0; i < ansLen; i++) {
-            if(test1[i] == answers[i]) count[0]++;
-            if(test2[i] == answers[i]) count[1]++;
-            if(test3[i] == answers[i]) count[2]++;
+        // 2. 채점
+        int aScore = 0;
+        int bScore = 0;
+        int cScore = 0;
+        
+        for(int i = 0; i < n; i++) {
+            if(aAnswer[i] == answers[i]) aScore++;
+            if(bAnswer[i] == answers[i]) bScore++;
+            if(cAnswer[i] == answers[i]) cScore++;
         }
         
-        // 3. 시험지 점수 비교 
-        // 값 3개 비교 
-        int maxScore = Math.max(count[0], Math.max(count[1], count[2]));
+        // 3. 최댓값
+        int maxScore = Math.max(aScore, Math.max(bScore, cScore));
         
-        if(maxScore == count[0]) answerArr.add(1);
-        if(maxScore == count[1]) answerArr.add(2);
-        if(maxScore == count[2]) answerArr.add(3);
+        ArrayList<Integer> lst = new ArrayList<>();
+        if(maxScore == aScore) lst.add(1);
+        if(maxScore == bScore) lst.add(2);
+        if(maxScore == cScore) lst.add(3);
         
-        // 4. 리스트 -> 배열 변환 
-        int[] answer = new int[answerArr.size()];
+        int[] answer = new int[lst.size()];
         for(int i = 0; i < answer.length; i++) {
-            answer[i] = answerArr.get(i);
+            answer[i] = lst.get(i);
         }
-        
         return answer;
     }
 }
