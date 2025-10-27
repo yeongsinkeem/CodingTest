bd15128f94b3a8691378fa54df91e6d322287cef
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-10-28 00:19:20 +0900

    [level 3] Title: 단어 변환, Time: 0.45 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,71 +1,62 @@
 import java.util.*;
 
 class Solution {
-    private String begin;
-    private String target;
-    
-    private ArrayList<Integer> counts;
-    private boolean[] visited;
-    private String[] words;
+    class Node {
+        String word;
+        int depth;
+        
+        public Node(String word, int depth) {
+            this.word = word;
+            this.depth = depth;
+        }
+    }
     
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        this.begin = begin;
-        this.target = target;
-        this.words = words;
         
-        counts = new ArrayList<>();
-        visited = new boolean[words.length];
-        
-        // 만약 words 안에 target이 없으면 바로 종료
+        // 전제 조건 - target이 words에 있을 것
         boolean exists = false;
-        for (String str : words) {
-            if (str.equals(target)) {
+        for(String str : words) {
+            if(str.equals(target)) {
                 exists = true;
                 break;
             }
         }
-        if (!exists) return 0;
+        if( !exists ) return 0;
         
-        // 1. DFS 시작 - begin 가지고
-        DFS(begin, 0);
+        // 1. queue & visited 준비
+        Queue<Node> queue = new LinkedList<>();
+        boolean[] visited = new boolean[words.length];
         
-        int[] countArr = new int[counts.size()];
-        for(int i = 0; i < countArr.length; i++) {
-            countArr[i] = counts.get(i);
-        }
+        queue.offer(new Node(begin, 0));
         
-        Arrays.sort(countArr);
-        
-        return countArr[0];
-    }
-    
-    public void DFS(String str, int depth) {
-        // 종료 조건 
-        if(str.equals(target)) {
-            counts.add(depth);
-            return;
-        }
-        
-        // 방문 / 미방문 확인 
-        for(int i = 0; i < words.length; i++) {
-            if( !visited[i] && check(str, words[i]) ) {
-                visited[i] = true;
-                DFS(words[i], depth + 1);
-                visited[i] = false;
+        // 2. BFS 시작
+        while(!queue.isEmpty()) {
+            Node curr = queue.poll();
+            String currWord = curr.word;
+            int currDepth = curr.depth;
+            
+            // target 찾았다면 
+            if(currWord.equals(target)) return currDepth;
+            
+            // target 못 찾았다면 탐색 
+            for(int i = 0; i < words.length; i++) {
+                // 미방문 + 한 번만에 갈 수 있다면 
+                if( !visited[i] && check(currWord, words[i]) ) {
+                    visited[i] = true;
+                    queue.offer(new Node(words[i], currDepth + 1));
+                }
             }
         }
+        return 0;
     }
     
-    // 한 번만에 갈 수 있는지 확인 
     public boolean check(String a, String b) {
         int diff = 0;
         for(int i = 0; i < a.length(); i++) {
-            if(diff > 1) return false;
+            if (diff > 1) return false;
             
-            if( a.charAt(i) != b.charAt(i) ) {
-                diff ++;
-            }
+            if( a.charAt(i) != b.charAt(i) ) diff++;
         }
         return diff == 1;
     }
