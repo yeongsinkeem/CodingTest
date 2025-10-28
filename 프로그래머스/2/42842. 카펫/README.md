bb6fedb6f67e18682a416c9d6d5ef59f9da2536d
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-10-28 18:44:09 +0900

    [level 2] Title: 카펫, Time: 9.45 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
 
 ### 성능 요약
 
-메모리: 96.8 MB, 시간: 0.06 ms
+메모리: 84.6 MB, 시간: 9.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 23일 12:59:38
+2025년 10월 28일 18:44:07
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,23 +2,22 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
+        // 1. 전체 개수 입력 
         int total = brown + yellow;
         
-        for(int i = 1; i <= total; i++) {
-            if(total % i == 0) {
-                int width = i;
-                int height = total / i;
+        // 2. 공약수 구하기
+        for(int i = 2; i <= total ; i++) {
+            if( total % i == 0 ) {
+                int a = i;
+                int b = total / i;
                 
-                // 일자 직사각형인지, 블럭형 직사각형인지 확인 
-                if( (width - 2) * (height -2) == yellow ) {
-                    if(width >= height) {
-                        answer[0] = width;
-                        answer[1] = height;
-                        return answer;
-                    }
+                if( (a - 2) * (b - 2) == yellow ) {
+                    answer[0] = a;
+                    answer[1] = b;
                 }
             }
         }
+        
         return answer;
     }
 }
