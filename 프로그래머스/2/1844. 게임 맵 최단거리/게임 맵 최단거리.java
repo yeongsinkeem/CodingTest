2772634f8d010444045a9edfefc7e3d8c202d69c
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-10-29 18:31:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.92 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,53 +1,50 @@
 import java.util.*;
 
 class Solution {
-    public static int[] dx = {0, 0, 1, -1};
-    public static int[] dy = {1, -1, 0, 0};
+    int[] dx = {0, 0, 1, -1};
+    int[] dy = {1, -1, 0, 0};
     
     public int solution(int[][] maps) {
-        // int answer = 1;
+        int answer = 0;
         int n = maps.length;
         int m = maps[0].length;
         
-        // q = 이동할(한) 좌표 넣을 거임 
-        Queue<int[]> q = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList<>();
         
-        // 방문 좌표 : 미방문(0), 방문(++1)
-        int[][] visited = new int[n][m];
+        // BFS 
+        // 1. 큐에 시작 좌표 넣기
+        queue.offer(new int[]{0, 0});
         
-        // 1. BFS 시작 
-        q.offer( new int[]{0, 0} );
-        visited[0][0] = 1;
-        
-        while(!q.isEmpty()) {
-            int[] currPos = q.poll();
-            int currX = currPos[0];
-            int currY = currPos[1];
+        while(!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int currX = curr[0];
+            int currY = curr[1];
             
-            // 2. 동서남북 탐색 
+            // 1. 동서남북 탐색 
             for(int i = 0; i < 4; i++) {
                 int newX = currX + dx[i];
                 int newY = currY + dy[i];
                 
-                // 3. 만약 좌표판 벗어난다면 
-                if(newX < 0 || newY < 0 || newX >= n || newY >= m) 
-                    continue;
+                // 2. maps 경계 벗어난다면
+                if (newX < 0 || newY < 0 || newX > n - 1 || newY > m - 1) continue;
                 
-                // 4. 만약 벽으로 막혀 있다면 
-                if(maps[newX][newY] == 0) 
-                    continue;
+                // 3. 벽으로 막혀 있다면 
+                if (maps[newX][newY] == 0) continue;
                 
-                // 5. 만약 미방문 좌표라면
-                if(visited[newX][newY] == 0) {
-                    // 이동할 수 있으니 큐에 좌표 넣어주기
-                    q.offer(new int[]{newX, newY});
-                    // 값 업데이트 
-                    visited[newX][newY] = visited[currX][currY] + 1;
+                // 4. 미방문 좌표라면 : maps 상에서 값이 0 또는 1일 것
+                // 하드 코딩 어떻게 ? 
+                if (maps[newX][newY] < 2) {
+                    // 값 업데이트
+                    // 이전 좌표 + 1
+                    maps[newX][newY] = maps[currX][currY] + 1;
+                    // 큐에 값 넣기 
+                    queue.offer(new int[]{newX, newY});
                 }
             }
         }
-        int answer = visited[n-1][m-1];
         
-        return (answer == 0) ? -1 : answer;
+        if( maps[n-1][m-1] < 2 ) return -1;
+        
+        return maps[n-1][m-1];
     }
 }
