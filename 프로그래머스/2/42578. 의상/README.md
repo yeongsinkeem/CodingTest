3998b4afbcfbb824a8be7eeab673229c830a78e1
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-10-29 11:16:34 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 79.4 MB, 시간: 0.13 ms
+메모리: 85.7 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 16일 00:08:11
+2025년 10월 29일 11:16:32
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,28 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
-        int count = 0;
-    
-        // 1. category 별로 저장
-        for(int i = 0; i < clothes.length; i++) {
-            String category = clothes[i][1];
-            map.put(category, map.getOrDefault(category, 0) + 1);
-        }
-        
-        // 2. answer 계산
-        // category == 1개라면
-        if(map.size() == 1) {
-            for(int value : map.values())
-                return value;
-        }
-        // category >= 2개라면
-        // answer = (A 카테고리 + 1) * (B 카테고리 + 1) ... - 1
         int answer = 1;
-        for(int value : map.values()) {
-            answer *= value + 1;
+        
+        // 1. category + type으로 map에 저장
+        Map<String, Integer> map = new HashMap<>();
+        
+        // 2. type의 개수 + 1이 type으로 할 수 있는 조합 수 
+        for(int i = 0; i < clothes.length; i++) {
+            map.put( clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         
-        return answer - 1;
+        // 3. 계산 
+        for(String key : map.keySet()) {
+            answer *= (map.get(key) + 1);
+        }
+        
+        /* 
+        for(String key : map.keySet()) {
+            System.out.println(key + " : " + map.get(key));
+        }
+        */ 
+        
+        // 3. 이때 전체 개수에서 아무것도 입지 않은 경우는 제외할 것.
+        answer -= 1;
+        return answer;
     }
 }
