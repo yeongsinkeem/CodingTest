646c58ab8f93f7d4a4df398b49ca0f407c9e4d8c
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-10-31 00:54:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.31 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -9,41 +9,39 @@ class Solution {
         int n = maps.length;
         int m = maps[0].length;
         
+        // 좌표가 들어갈 큐 + 방문 배열
         Queue<int[]> queue = new LinkedList<>();
+        // int[] visited = new int[n * m];
         
-        // BFS 
-        // 1. 큐에 시작 좌표 넣기
+        // 1. 시작좌표(0, 0) Queue에 넣어주기
         queue.offer(new int[]{0, 0});
         
-        while(!queue.isEmpty()) {
+        while( !queue.isEmpty() ) {
             int[] curr = queue.poll();
             int currX = curr[0];
             int currY = curr[1];
             
-            // 1. 동서남북 탐색 
             for(int i = 0; i < 4; i++) {
                 int newX = currX + dx[i];
                 int newY = currY + dy[i];
                 
-                // 2. maps 경계 벗어난다면
-                if (newX < 0 || newY < 0 || newX > n - 1 || newY > m - 1) continue;
+                // 2. maps 경계 벗어난다면 > 탈출
+                if( newX < 0 || newY < 0 || newX >= n || newY >= m )
+                    continue;
                 
-                // 3. 벽으로 막혀 있다면 
-                if (maps[newX][newY] == 0) continue;
+                // 3. 막혀있다면 > 탈출 
+                if( maps[newX][newY] == 0 )
+                    continue;
                 
-                // 4. 미방문 좌표라면 : maps 상에서 값이 0 또는 1일 것
-                // 하드 코딩 어떻게 ? 
-                if (maps[newX][newY] < 2) {
-                    // 값 업데이트
-                    // 이전 좌표 + 1
-                    maps[newX][newY] = maps[currX][currY] + 1;
-                    // 큐에 값 넣기 
+                // 4. 미방문 좌표라면 
+                if( maps[newX][newY] <= 1 ) {
+                    maps[newX][newY] = maps[currX][currY] + 1;  
                     queue.offer(new int[]{newX, newY});
                 }
             }
         }
         
-        if( maps[n-1][m-1] < 2 ) return -1;
+        if( maps[n-1][m-1] == 1 ) return -1;
         
         return maps[n-1][m-1];
     }
