3df20921a3270a4c84a8c73497b3662f906774c7
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-10-30 17:09:54 +0900

    [level 2] Title: 올바른 괄호, Time: 13.85 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,25 +2,26 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
+        if( !s.contains("(") ) return false;
         
-        Stack<Character> stack = new Stack<>();
+        char[] c = s.toCharArray();
+        Queue<Character> queue = new LinkedList<>();
+        int open = 0;
+        int close = 0;
         
-        char[] sChar = s.toCharArray();
-        for(int i = 0; i < sChar.length; i++) {
-            // 열린 괄호라면 push
-            if(sChar[i] == '(') {
-                stack.push(sChar[i]);
+        // 1. 열린 괄호라면 큐에 넣기
+        for(int i = 0; i < c.length; i++) {
+            if( c[i] == '(' ) {
+                queue.offer(c[i]);
+                open++;
             }
-            // 닫힌 괄호라면 pop
             else {
-                if(stack.isEmpty()) {
-                    return false;
-                }
-                stack.pop();
+                queue.poll();
+                close++;
             }
         }
         
-        return stack.isEmpty();
+        if( queue.isEmpty() && (open == close) )return true;
+        else return false;
     }
 }
