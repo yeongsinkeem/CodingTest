9af87b03fefca0db4f124bf6018c526cf95d634c
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-02 13:51:31 +0900

    [level 2] Title: 피로도, Time: 2.81 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,32 +1,45 @@
-import java.util.*;
-
 class Solution {
     boolean[] visited;
     int[][] dungeons;
-    int max = 0;
+    int count;
+    int maxCount = 0;
+    int k;
     
     public int solution(int k, int[][] dungeons) {
+        int answer = -1;
         this.dungeons = dungeons;
+        this.k = k;
         visited = new boolean[dungeons.length];
         
-        DFS(k, 0); // 시작 피로도와 방문 횟수 전달
-        return max;
+        // 1. DFS 시작
+        DFS(visited, 0);
+        
+        return maxCount;
     }
     
-    public void DFS(int k, int count) {
-        // 현재 탐험 가능한 던전 수로 최대값 갱신
-        max = Math.max(max, count);
+    public void DFS(boolean[] v, int count) {
+        //  1. 종료조건
+        if(maxCount <= count) {
+            maxCount = count;
+        }
         
-        for (int i = 0; i < dungeons.length; i++) {
-            int minNeed = dungeons[i][0];
-            int consume = dungeons[i][1];
-            
-            // 아직 방문 안했고, 현재 피로도로 입장 가능할 때
-            if (!visited[i] && k >= minNeed) {
-                visited[i] = true;
-                DFS(k - consume, count + 1); // 다음 탐색 (피로도 감소)
-                visited[i] = false;           // 백트래킹
+        // 2. 미방문 노드 && 체력 조건 충족 
+        // 반복문 동안 재귀 
+        for(int i = 0; i < dungeons.length; i++) {
+            if( !v[i] && check(k, dungeons[i][0]) ) {
+                k -= dungeons[i][1];
+                v[i] = true;
+                
+                DFS(v, count + 1);
+                
+                v[i] = false;
+                k += dungeons[i][1];
             }
         }
+    }
+    
+    public boolean check(int a, int b) {
+        if( a >= b ) return true;
+        else return false;
     }
 }
