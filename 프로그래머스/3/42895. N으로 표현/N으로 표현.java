910131221448bcf74eed33f81fe91381f7d2340b
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-07 16:07:21 +0900

    [level 3] Title: N으로 표현, Time: 33.57 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -3,52 +3,46 @@ import java.util.*;
 class Solution {
     public int solution(int N, int number) {
         
-        if( N == number ) {
-            return 1;
-        }
+        if( N == number ) return 1;
         
-        // 1. 요소가 HashSet DP 리스트 만들기
-        List<Set<Integer>> dpSets = new ArrayList<>();
+        // 1. dp 만들기 
+        List<Set<Integer>> dp = new ArrayList<>();
         for(int i = 0; i <= 8; i++) {
-            dpSets.add(new HashSet<>());
+            dp.add(new HashSet<>());
         }
         
-        // 2. k = 1일 때 초기값 설정
-        dpSets.get(1).add(N);
+        // 2. dp[1] = N
+        dp.get(1).add(N);
         
-        // 3. k = 2 ~ 8까지 탐색 
+        // 3. i = 2 ~ 8까지 dp 만들기
         for(int k = 2; k <= 8; k++) {
-            // 4. 문자열 이어 붙이기 ex. 5, 55, 555
+            // 4. 숫자 붙이기
             StringBuilder sb = new StringBuilder();
-            for(int count = 0; count < k; count++) {
+            for(int i = 0; i < k; i++) {
                 sb.append(N);
             }
-            dpSets.get(k).add(Integer.parseInt(sb.toString()));
+            dp.get(k).add(Integer.parseInt(sb.toString()));
             
-            // 5. i와 j 탐색하면서 dpSets에 값 추가하기
+            // 5. 사칙연산
+            // dp[5] = dp[1] + dp[4], dp[2] + dp[3]
             for(int i = 1; i < k; i++) {
                 int j = k - i;
                 
-                // N을 i번 사용
-                Set<Integer> set1 = dpSets.get(i);
-                Set<Integer> set2 = dpSets.get(j);
-                
-                for(int a : set1) {
-                    for(int b : set2) {
-                        dpSets.get(k).add(a + b);
-                        dpSets.get(k).add(a - b);
-                        dpSets.get(k).add(a * b);
-                        
-                        if( b != 0 ) dpSets.get(k).add(a / b);
+                for(int a : dp.get(i)){
+                    for(int b : dp.get(j)) {
+                        dp.get(k).add(a + b);
+                        dp.get(k).add(a - b);
+                        dp.get(k).add(a * b);
+                        if( b != 0 ) {
+                            dp.get(k).add(a / b);
+                        }
                     }
                 }
             }
             
-            // 6. k번째 HashSet에 number있는지 확인
-            if(dpSets.get(k).contains(number)) return k;
+            if( dp.get(k).contains(number) ) return k;
         }
-        
-        
+    
         return -1;
     }
 }
