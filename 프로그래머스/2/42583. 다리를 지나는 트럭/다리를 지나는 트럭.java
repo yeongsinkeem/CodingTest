b51c15bc2b93e37ad617721f2f120a717fa2039b
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-07 11:28:38 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 35.58 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,44 +3,56 @@ import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        
-        // 트럭 큐
-        Queue<Integer> truck = new LinkedList<>();
-        // 다리 큐
-        Queue<Integer> bridge = new LinkedList<>();
-        
-        // 1. 트럭 큐에 트럭 넣기
-        for(int i = 0; i < truck_weights.length; i++) {
-            truck.offer(truck_weights[i]);
-        }
-        
-        // 2. 다리 큐에 값 채워넣기
+        // 1. 다리 큐
+        Queue<Integer> bridgeQ = new LinkedList<>();
         for(int i = 0; i < bridge_length; i++) {
-            bridge.offer(0);
+            bridgeQ.offer(0);
         }
         
-        int truckSum = 0;
-        // 3. 시간의 흐름에 따라 트럭을 다리에 올리고 -> 건너기 
-        while(!truck.isEmpty()) {
-            answer ++;
-            
-            // 3-1. 트럭 다리에 내리기
-            int exited = bridge.poll();
-            truckSum -= exited;
-            
-            // 3-2. 트럭 다리에 올리기 
-            // 무게가 된다면 
-            if(truckSum + truck.peek() <= weight) {
-                int entering = truck.poll();
-                truckSum += entering;
-                bridge.offer(entering);
-            }
-            // 무게가 되지 않는다면 
-            else {
-                bridge.offer(0);
-            }
-            
+        // 2. 대기 큐
+        Queue<Integer> waitQ = new LinkedList<>();
+        for(int i = 0; i < truck_weights.length; i++) {
+            waitQ.offer(truck_weights[i]);
         }
-        return answer + bridge_length;
+        
+        // 3. 초기 설정 
+        int completed = 0;
+        int time = 0;
+        int sum = 0;
+        
+        // 3. 모든 트럭이 다 지나갈 때까지
+        while(completed < truck_weights.length) {
+            time ++;
+            
+            // 4. 트럭(혹은 0) 나옴
+            int item = bridgeQ.poll();
+            
+            // 5. 실제 나온게 트럭이라면
+            if(item != 0) {
+                sum -= item;
+                completed++;
+            }
+            
+            // 6. 트럭 다리에 올리기
+            if( !waitQ.isEmpty() ) {
+                // 7. 다리에 올라갈 수 있는 경우
+                if( sum + waitQ.peek() <= weight ) {
+                    int leaving = waitQ.poll();
+                    sum += leaving;
+                    bridgeQ.offer(leaving);
+                } 
+                
+                // 8. 다리에 올라갈 수 없는 경우 
+                else {
+                    bridgeQ.offer(0);
+                }
+            } 
+            // 대기 트럭이 없어도 트럭은 다리 지나가야 함
+            else {
+                bridgeQ.offer(0);
+            }
+        }
+        
+        return time;
     }
 }
