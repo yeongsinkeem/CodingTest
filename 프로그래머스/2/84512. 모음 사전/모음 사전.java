7981c6b5d543e674fc95c19e2ce7a4787a06fb2d
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-11 18:19:24 +0900

    [level 2] Title: 모음 사전, Time: 7.06 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,32 +1,30 @@
 import java.util.*;
 
 class Solution {
-    static List<String> dic = new ArrayList<>();
-    static String[] alArr = {"A", "E", "I", "O", "U"};
+    String curr;
+    List<String> lst;
+    String[] aeiou = {"A", "E", "I", "O", "U"};
     
     public int solution(String word) {
         int answer = 0;
         
-        String curr = "";
+        // 1. 리스트 준비
+        lst = new ArrayList<>();
+        
+        curr = "";
         DFS(curr);
         
-        return dic.indexOf(word) + 1;
+        return lst.indexOf(word) + 1;
     }
     
-    public void DFS(String str) {
-        // 1. 단어 먼저 추가
-        if(!str.isEmpty()) {
-            dic.add(str);
-        }
+    public void DFS(String s) {
+        // 1. 종료조건
+        if( s.length() > 5 ) return;
         
-        // 2. 종료 조건 -> 단어 길이 5
-        if(str.length() == 5) {
-            return;
-        }
+        if( !s.isEmpty() ) lst.add(s);
         
-        // 3. 알파벳 붙이기
-        for(int i = 0; i < alArr.length; i++) {
-            DFS(str + alArr[i]);
+        for(int i = 0; i < 5; i++) {
+            DFS( s + aeiou[i] );
         }
     }
 }
