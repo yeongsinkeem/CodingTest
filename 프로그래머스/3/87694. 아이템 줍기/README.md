74f3e80bcd8035962977086f7e0d8255d18d228a
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-13 17:33:24 +0900

    [level 3] Title: 아이템 줍기, Time: 1.25 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/README.md
+++ 프로그래머스/3/87694. 아이템 줍기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 아이템 줍기 - 87694 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694?language=java) 
 
 ### 성능 요약
 
-메모리: 88.1 MB, 시간: 1.32 ms
+메모리: 82 MB, 시간: 1.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 05일 23:39:30
+2025년 11월 13일 17:33:21
 
 ### 문제 설명
 
--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,96 +1,96 @@
 import java.util.*;
 
 class Solution {
-    final int EMPTY = 0;
-    final int PATH = 1;
-    final int WALL = -1;
+    int[][] maps = new int[102][102];
+    int[][] visited = new int[102][102];
     
-    int[][] map = new int[102][102];
-    int[][] dist = new int[102][102];
-    
-    int[] dx = {0, 0, -1, 1};
+    int[] dx = {0, 0, 1, -1};
     int[] dy = {1, -1, 0, 0};
     
-    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-    
-        // 1. 좌표 생성
-        createMap(rectangle);
-        
-        // 2. BFS를 위한 초기화
-        int startX = characterX * 2;
-        int startY = characterY * 2;
-        int targetX = itemX * 2;
-        int targetY = itemY * 2;
-        
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{startX, startY});
-        
-        // 3. dist(방문) 배열 초기화 
-        for(int i = 0; i < 102; i++) {
-            for(int j = 0; j < 102; j++) {
-                // 벽 또는 방문 불가
-                if(map[i][j] != PATH) dist[i][j] = -1;
-                // 방문 가능
-                else dist[i][j] = -2;
-            }
-        }
-        
-        // 4. 시작점 설정 
-        dist[startX][startY] = 1;
-        
-        while( !queue.isEmpty() ) {
-            int[] curr = queue.poll();
-            int currX = curr[0];
-            int currY = curr[1];
-            
-            // 5. 종료조건
-            if( currX == targetX && currY == targetY ) 
-                return (dist[currX][currY] - 1) / 2;
-            
-            for(int i = 0; i < 4; i++) {
-                int newX = currX+ dx[i];
-                int newY = currY + dy[i];
-                
-                // 1. rectangle 안에 좌표 있어야 함
-                if(newX < 0 || newY < 0 || newX > 101 || newY > 101) continue;
-                
-                // 2. 갈 수 있어야 함
-                if(map[newX][newY] == WALL) continue;
-                
-                // 3. 미방문이어야 함
-                if(dist[newX][newY] == -2) {
-                    dist[newX][newY] = dist[currX][currY] + 1;
-                    queue.offer(new int[]{newX, newY});
-                }
-            }
-        }
-        return 0;
-    }
+    final int PATH = 1;
+    final int WALL = 2;
+    final int EMPTY = -1;
     
     public void createMap(int[][] rectangle) {
-        // 1. 각 사각형에 대해 모든 좌표 2배
-        for(int[] rect : rectangle) {
-            int x1 = rect[0] * 2;
-            int y1 = rect[1] * 2;
-            int x2 = rect[2] * 2;
-            int y2 = rect[3] * 2;
-            
-            for(int i = x1; i <= x2; i++) {
-                for(int j = y1; j <= y2; j++) {
-                    // 2. 벽의 내부인지 테두리인지 판별 
-                    // WALL : 내부, PATH : 테두리
-                    if( i > x1 && i < x2 && j > y1 && j < y2 ) {
-                        map[i][j] = WALL;
-                    }
-                    // 이때 테두리가 다른 사각형의 내부가 아닌 경우에만 길
-                    else {
-                        if( map[i][j] != WALL ) {
-                            map[i][j] = PATH;
+            for(int[] rec : rectangle) {
+                int startX = rec[0] * 2;
+                int startY = rec[1] * 2;
+                int endX = rec[2] * 2;
+                int endY = rec[3] * 2;
+                
+                for(int a = startX; a <= endX; a++) {
+                    for(int b = startY; b <= endY; b++) {
+                        // 이렇게 되면 경계면도 PATH가 됨 -> 따라서 WALL 처리 다시 
+                        if( a == startX || a == endX || b == startY || b == endY ) {
+                            maps[a][b] = PATH; 
                         }
                     }
                 }
             }
+        
+            for(int[] rec : rectangle) {
+                int startX = rec[0] * 2;
+                int startY = rec[1] * 2;
+                int endX = rec[2] * 2;
+                int endY = rec[3] * 2;
+                
+                for(int a = startX; a <= endX; a++) {
+                    for(int b = startY; b <= endY; b++) {
+                        // 이렇게 되면 경계면도 PATH가 됨 -> 따라서 WALL 처리 다시 
+                        if( a > startX && a < endX && b > startY && b < endY ) {
+                            maps[a][b] = WALL; 
+                    }
+                }
+            }
+        }
+    }
+    
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        // 1. 지도 그리기
+        createMap(rectangle);
+        
+        // 2. 변수 준비
+        int cX = characterX * 2;
+        int cY = characterY * 2;
+        int iX = itemX * 2;
+        int iY = itemY * 2;
+        
+        // 3. BFS 준비
+        Queue<int[]> queue = new LinkedList<>();
+        
+        for(int i = 0; i < 102; i++) {
+            for(int j = 0; j < 102; j++) {
+                visited[i][j] = 0;
+            }
         }
         
+        queue.offer( new int[]{cX, cY} );
+        
+        // 4. 시작점은 1로 표시 
+        visited[cX][cY] = 1;
+        
+        // 5. BFS 시작
+        while(!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int currX = curr[0];
+            int currY = curr[1];
+            
+            if(currX == iX && currY == iY) return (visited[currX][currY] - 1) / 2;
+            
+            for(int i = 0; i < 4; i++) {
+                int newX = curr[0] + dx[i];
+                int newY = curr[1] + dy[i];
+                
+                // 미방문 + PATH + maps에 존재 
+                if( newX > 0 && newX < 102 && newY > 0 && newY < 102 ) {
+                    if( maps[newX][newY] == PATH && visited[newX][newY] == 0 ) {
+                        queue.offer(new int[]{newX, newY});
+                        visited[newX][newY] = visited[currX][currY] + 1;
+                    }
+                }
+            }
+        }
+        
+        return 0;
     }
 }
