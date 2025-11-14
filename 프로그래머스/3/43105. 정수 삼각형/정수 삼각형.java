4be1691c6491e12bd1190e61d622c7d1f25b4853
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-14 11:53:21 +0900

    [level 3] Title: 정수 삼각형, Time: 13.32 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,20 +1,19 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] triangle) {
         int answer = 0;
+        int len = triangle.length - 1;
         
-        int N = triangle.length;
-        
-        for(int i = N - 2; i >= 0; i--) {
-            for(int j = 0; j <= i; j++) {
-                triangle[i][j] = triangle[i][j] + 
-                    Math.max(triangle[i+1][j], triangle[i+1][j+1]);
+        // 맨마지막은 계산할 필요 x
+        for( int i = len - 1; i >= 0; i-- ) {
+            for( int j = 0; j <= triangle[i].length - 1; j++ ) {
+                int left = triangle[i + 1][j];
+                int right = triangle[i + 1][j + 1];
+                
+                triangle[i][j] = triangle[i][j] + Math.max(right, left);
+                // System.out.println(triangle[i][j]);
             }
         }
         
-        answer = triangle[0][0];
-        
-        return answer;
+        return triangle[0][0];
     }
 }
