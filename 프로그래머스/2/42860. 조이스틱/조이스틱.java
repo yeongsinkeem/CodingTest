3b36096aaca325524564711e24124c191143c2f3
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-16 11:32:14 +0900

    [level 2] Title: 조이스틱, Time: 0.08 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,27 +1,32 @@
 class Solution {
     public int solution(String name) {
-        int answer = 0;
-        char[] nameArr = name.toCharArray();
+        char[] c = name.toCharArray();
+        int alpha = 0;
         
-        for (int i = 0; i < name.length(); i++) {
-            int t = nameArr[i] - 'A';
-            answer += Math.min(t, 26 - t);
+        // 1. 알파벳 계산
+        for(int i = 0; i < c.length; i++) {
+            int diff = c[i] - 'A';
+            alpha += Math.min(diff, 26 - diff);
         }
         
-        
+        // 2. 거리 계산
+        // 2-1. 쭉 가기
         int move = name.length() - 1;
-        for(int i = 0; i < name.length(); i++) {
+        
+        for(int i = 0; i < c.length; i++) {
             int next = i + 1;
             
-            // 연속된 A 건너뛰기 
-            while( next < name.length() && nameArr[next] == 'A' ) {
-                next ++;
+            // 연속된 A 건너뛰기
+            while( next < c.length && c[next] == 'A') {
+                next++;
             }
+            // 2-2. 순 -> 역
+            move = Math.min(move, i + i + (c.length - next));
             
-            move = Math.min(move, i * 2 + name.length() - next);
-            move = Math.min(move, (name.length() - next) * 2 + i);
+            // 2-3. 역 -> 순
+            move = Math.min(move, (c.length - next) * 2 + i );
         }
         
-        return answer + move;
+        return alpha + move;
     }
 }
