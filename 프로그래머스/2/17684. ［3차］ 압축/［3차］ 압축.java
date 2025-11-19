abfba11b796cd80bd7f50a7b00a8388b16ae0464
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-20 00:30:57 +0900

    [level 2] Title: [3차] 압축, Time: 15.42 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,41 +2,41 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
+        List<Integer> answerLst = new ArrayList<>();
         
-        // 1. 사전 초기화 
+        // 1. 길이가 1인 모든 단어 포함하도록 사전 초기화
         Map<String, Integer> map = new HashMap<>();
         for(int i = 0; i < 26; i++) {
-            map.put( ((char)('A' + i) + ""), i + 1 );
+            map.put( ((char)('A' + i)) + "", i + 1);
         }
         
-        List<Integer> answerLst = new ArrayList<>();
-        int dictIdx = 27;
+        int idx = 27;
         
-        // 2. msg 순회하면서
         for(int i = 0; i < msg.length(); i++) {
             String w = msg.charAt(i) + "";
             
-            // 3. 다음 글자 붙인게 사전에 있다면 -> 다음 글자 붙이기 
-            while( i + 1 < msg.length() && map.containsKey(w + msg.charAt(i + 1)) ) { 
-                w += msg.charAt(i+1);
-                i++; // 인덱스 증가 
+            // 2. 붙인 문자열 map에 존재하는지, 안 하는지 확인
+            // 계속 붙여야 하기 때문
+            while( i + 1 < msg.length() && map.containsKey(w + msg.charAt(i+1) + "" ) ) {
+                w += msg.charAt(i+1) + "";
+                i ++;
             }
-            // 4. 사전에 없다면 출력
+            
+            // 3. 붙인 게 없다면 출력
             answerLst.add(map.get(w));
             
-            
-            // 5. 다음 글자가 남아있다면 등록
+            // 4. 새로운 문자열 등록 
             if( i + 1 < msg.length() ) {
-                String c = msg.charAt(i + 1) + "";
-                map.put( w + c, dictIdx++ );
+                map.put( w + (msg.charAt(i+1) + ""), idx++ );
             }
         }
         
-        // 6. 마지막 출력
         int[] answer = new int[answerLst.size()];
         for(int i = 0; i < answer.length; i++) {
             answer[i] = answerLst.get(i);
         }
+        
+        
         return answer;
     }
 }
