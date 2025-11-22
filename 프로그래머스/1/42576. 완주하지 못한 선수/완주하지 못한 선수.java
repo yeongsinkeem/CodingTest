615aabd7b92dd91a3c3be2e8173d6cd92f09a688
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-22 09:59:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 105.47 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -6,28 +6,26 @@ class Solution {
         
         Map<String, Integer> map = new HashMap<>();
         
-        // 1. participant 원소 map에 저장
-        for(String name : participant) {
-            map.put(name, map.getOrDefault(name, 0) + 1);
+        
+        // 1. participant 순회하면서 map에 이름, 횟수 넣기
+        for(String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
         
-        // 2. completion 순회하면서 count 계산
-        for(int i = 0; i < completion.length; i++) {
-            // map에 해당 key 가 있다면 
-            if ( map.containsKey(completion[i]) ) {
-                int count = map.get(completion[i]);
-                map.put(completion[i], count - 1);
+        // 2. completion 순회하면서 완주했으면 횟수 감소하기
+        for(String c : completion) {
+            if( map.containsKey(c) ) {
+                int cnt = map.get(c) - 1;
+                // 제거 -> 다시 넣기
+                map.remove(c);
+                map.put(c, map.getOrDefault(c, cnt));
             }
         }
         
-        // 3. key로 value 접근 
-        // System.out.println(map.get("leo"));
-        for(String name : map.keySet()) {
-            if( map.get(name) == 1 ) {
-                return name;
-            }
+        for(String p : map.keySet()) {
+            if( map.get(p) == 1 ) return p;
         }
-    
+        
         return answer;
     }
 }
