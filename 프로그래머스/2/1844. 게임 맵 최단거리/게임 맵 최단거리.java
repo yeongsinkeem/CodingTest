a0bcf5f7991bf4678ac1197ac1a99e2edb162e98
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-22 22:20:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.15 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,39 +1,61 @@
 import java.util.*;
 
 class Solution {
+    int[] dx = {1, -1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+    
+    int[][] maps;
+    boolean[][] visited;
+    // 좌표 넣을 큐
+    Queue<int[]> q = new LinkedList<>();
+    
+    int n;
+    int m;
+    
     public int solution(int[][] maps) {
-        // int answer = 0;
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {1, -1, 0, 0};
-        int n = maps.length;
-        int m = maps[0].length; 
+        int answer = 0;
+        this.maps = maps;
         
-        // 1. 좌표 Queue 준비 
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {0, 0});
+        n = maps.length;
+        m = maps[0].length;
         
+        visited = new boolean[n][m];
+        
+        // 1. 좌표 넣어주기 
+        q.offer(new int[]{0, 0});
+        visited[0][0] = true;
+        BFS();
+        
+        if( maps[n-1][m-1] <= 1 ) return -1;
+        
+        return maps[n-1][m-1];
+    }
+    
+    public void BFS() {
         while( !q.isEmpty() ) {
             int[] curr = q.poll();
             int currX = curr[0];
             int currY = curr[1];
             
-            for(int i = 0; i < 4; i++) {
+            // 동서남북 탐색
+            for( int i = 0; i < 4; i++ ) {
                 int newX = currX + dx[i];
                 int newY = currY + dy[i];
                 
-                // maps 내부에 존재 
+                // 1. 해당 좌표는 maps 경계 안에 존재할 것
                 if( newX < 0 || newY < 0 || newX >= n || newY >= m ) continue;
                 
-                // 미방문 + 갈 수 있어야
+                // 2. 해당 좌표는 미방문일 것
+                if( visited[newX][newY] ) continue;
+                
+                // 3. 벽으로 막혀있지 않다면
                 if( maps[newX][newY] == 1 ) {
-                    q.offer(new int[] {newX, newY});
                     maps[newX][newY] = maps[currX][currY] + 1;
+                    visited[newX][newY] = true;
+                    
+                    q.offer(new int[]{newX, newY});
                 }
             }
         }
-        if( maps[n-1][m-1] > 1 ) return maps[n-1][m-1];
-        else return -1;
-        
-        // return answer;
     }
 }
