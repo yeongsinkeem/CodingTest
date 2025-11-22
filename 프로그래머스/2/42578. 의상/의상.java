87bdb2477d5a5f209c0de04b0cf407ee5c401900
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-22 13:26:45 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,30 +1,21 @@
+// 13 : 18 ~ 
 import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        
-        // 1. category + type으로 map에 저장
         Map<String, Integer> map = new HashMap<>();
         
-        // 2. type의 개수 + 1이 type으로 할 수 있는 조합 수 
+        // 1. 카테고리 별로 종류 개수 저장
         for(int i = 0; i < clothes.length; i++) {
-            map.put( clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+            String category = clothes[i][1];
+            
+            map.put(category, map.getOrDefault(category, 0) + 1);
         }
         
-        // 3. 계산 
-        for(String key : map.keySet()) {
-            answer *= (map.get(key) + 1);
+        for(String category : map.keySet()) {
+            answer *= map.get(category) + 1;
         }
-        
-        /* 
-        for(String key : map.keySet()) {
-            System.out.println(key + " : " + map.get(key));
-        }
-        */ 
-        
-        // 3. 이때 전체 개수에서 아무것도 입지 않은 경우는 제외할 것.
-        answer -= 1;
-        return answer;
+        return answer - 1;
     }
 }
