9f6ed53dae0d7cac40faf11a3c3098cd9f063171
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-11-22 14:06:12 +0900

    [level 2] Title: 주차 요금 계산, Time: 7.91 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,90 +1,95 @@
 import java.util.*;
-import java.text.*;
 
 class Solution {
+    static String in = "IN";
+    static String out = "OUT";
+    
     public int[] solution(int[] fees, String[] records) {
-        Map<String, Integer> timeMap = new HashMap<>();
-        Map<String, String> inCar = new HashMap<>();
         
-        // 1. records 순회하면서 번호와 누적시간 구하기 
+        // 1. 입차 map 생성
+        Map<String, String> inCarMap = new HashMap<>();
+        
+        // 2. 총 주차 시간 map 생성
+        Map<String, Integer> totalTimeMap = new HashMap<>();
+        
+        // 3. records 순회하면서 IN -> 입차 map에 넣기
         for(String rec : records) {
-            String time = rec.split(" ")[0];
-            String car = rec.split(" ")[1];
-            String inOut = rec.split(" ")[2];
-            
-            if(inOut.equals("IN")) {
-                inCar.put(car, time);
+            // 입차량이라면 -> 입차 map에 넣기
+            if( rec.split(" ")[2].equals(in) ) {
+                String inCarNum = rec.split(" ")[1];
+                String inCarTime = rec.split(" ")[0];
+                inCarMap.put(inCarNum, inCarTime);
             }
+            
+            // 출차량이라면 -> 시간 계산 후 입차 map에서 제거 
             else {
-                String inTime = inCar.get(car);
-                int diff = timeCalculate(inTime, time);
-                timeMap.put(car, timeMap.getOrDefault(car, 0) + diff);
+                String outCarNum = rec.split(" ")[1];
                 
-                inCar.remove(car);
+                String inCarTime = inCarMap.get(outCarNum);
+                String outCarTime = rec.split(" ")[0];
+                
+                int totalTime = calculateTime(inCarTime, outCarTime);
+                totalTimeMap.put(outCarNum, totalTimeMap.getOrDefault(outCarNum, 0) + totalTime);
+                inCarMap.remove(outCarNum);
             }
         }
         
-        // 2. 출차 기록이 없는 차량도 계산
-        for(String car : inCar.keySet()) {
-            String inTime = inCar.get(car);
-            int diff = timeCalculate(inTime, "23:59");
-            timeMap.put(car, timeMap.getOrDefault(car, 0) + diff);
+        // 4. 만약 입차량에 차량이 남아있다면 23:59로 계산
+        for( String inCar : inCarMap.keySet() ) {
+            String inCarTime = inCarMap.get(inCar);
+            String endTime = "23:59";
+                
+            int totalTime = calculateTime(inCarTime, endTime);
+            totalTimeMap.put(inCar, totalTimeMap.getOrDefault(inCar, 0) + totalTime);
         }
         
-        // 3. 차량번호 작은 순으로 정렬하여 요금 계산
-        List<String> carFees = new ArrayList<>(timeMap.keySet());
-        Collections.sort(carFees); // 기본 오름차순
-        
-        int[] answer = new int[carFees.size()];
-        
-        // timeMap에 접근하여 요금 계산 
-        /*
-        for(int i = 0; i < carFees.size(); i++) {
-            String car = carFees.get(i);
-            int totalTime = timeMap.get(car);
-            
-            int fee = calculatePrice(fees, totalTime);
-            answer[i] = fee;
-        }
-        */
+        List<String> carLst = new ArrayList<>(totalTimeMap.keySet());
+        Collections.sort(carLst);
+        int[] answer = new int[carLst.size()];
         
         int idx = 0;
-        for(String num : carFees) {
-            int totalMinutes = timeMap.get(num);
-            int price = calculatePrice(fees, totalMinutes);
-            answer[idx] = price;
+        
+        // fees[0] = 기본 시간
+        // fees[1] = 기본 요금
+        // fees[2] = 단위 시간
+        // fees[3] = 단위 요금 
+        for(String car : carLst) {
+            int totalTime = totalTimeMap.get(car);
+            int totalPrice = 0;
+            
+            // 기본시간 넘는다면
+            if( totalTime > fees[0] ) {
+                int extraTime = (totalTime - fees[0]) / fees[2];
+                // 올림 처리 
+                if( (totalTime - fees[0]) % fees[2] != 0 ) extraTime++;
+                
+                extraTime *= fees[3];
+                
+                totalPrice = fees[1] + extraTime;
+            }
+            
+            // 기본시간 넘지 않는다면
+            else {
+                totalPrice = fees[1];
+            }
+            answer[idx] = totalPrice;
             idx++;
         }
-        
         
         return answer;
     }
     
-    public int timeCalculate(String start, String end) {
-        try {
-            SimpleDateFormat format = new SimpleDateFormat("HH:mm");
-
-            Date date1 = format.parse(start);
-            Date date2 = format.parse(end);
-
-            long diff = date2.getTime() - date1.getTime();
-            return (int) (diff / 60000);
-        } catch (Exception e) {
-            return 0;
-        }
-    }
-    
-    public int calculatePrice(int[] fees, int time) {
-        int baseTime = fees[0];
-        int baseFee = fees[1];
-        int unitTime = fees[2];
-        int unitFee = fees[3];
-
-        if (time <= baseTime) return baseFee;
-
-        int extra = time - baseTime;
-        int units = (extra + unitTime - 1) / unitTime; // 올림
-
-        return baseFee + units * unitFee;
+    public int calculateTime(String start, String end) {
+        String[] startTime = start.split(":");
+        int startHour = Integer.parseInt(startTime[0]);
+        int startMinute = Integer.parseInt(startTime[1]);
+        int startTotalTime = startHour * 60 + startMinute;
+        
+        String[] endTime = end.split(":");
+        int endHour = Integer.parseInt(endTime[0]);
+        int endMinute = Integer.parseInt(endTime[1]);
+        int endTotalTime = endHour * 60 + endMinute;
+        
+        return endTotalTime - startTotalTime;
     }
 }
