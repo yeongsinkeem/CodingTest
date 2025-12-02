a6688f0367a54e5074d6d4e460cf034878e66565
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-12-02 19:18:36 +0900

    [level 2] Title: 롤케이크 자르기, Time: 101.31 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,36 +3,30 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        
-        int len = topping.length;
-        if( len < 2 ) return 0;
-        
         Set<Integer> set = new HashSet<>();
+        int n = topping.length; 
         
-        // 1. 철수의 토핑 종류 세기. 0 ~ len - 1
-        int[] aLst = new int[len];
-        for(int i = 0; i < len; i++) {
+        // 1. 자르는 지점 별로 토핑 개수 저장하기 
+        int[] aArr = new int[n];
+        for(int i = 0; i < n; i++) {
             set.add(topping[i]);
-            aLst[i] = set.size();
+            aArr[i] = set.size();
         }
         set.clear();
         
-        // 2. 동생의 토핑 종류 세기. len - 1 ~ 0 
-        int[] bLst = new int[len];
-        for(int i = len - 1; i >= 0; i--) {
+        
+        int[] bArr = new int[n];
+        for(int i = n-1; i >= 0; i--) {
             set.add(topping[i]);
-            bLst[i] = set.size();
+            bArr[i] = set.size();
         }
         
-        // 3. 자르는 지점을 기준으로 종류 수 비교
-        for(int i = 1; i < len; i++) {
-            int aKinds = aLst[i-1];
-            int bKinds = bLst[i];
-            
+        for(int i = 1; i < aArr.length; i++) {
+            // 비교할 때는 0~i-1, i~끝까지 이런식으로 비교해야 함
+            int aKinds = aArr[i-1];
+            int bKinds = bArr[i];
             if( aKinds == bKinds ) answer++;
         }
-        
-        
         return answer;
     }
 }
