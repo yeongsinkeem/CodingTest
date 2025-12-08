1238f877eaa6963fc730d3361f849b6c422e339f
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-12-09 00:23:31 +0900

    [level 2] Title: 프로세스, Time: 2.02 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,47 +1,43 @@
 import java.util.*;
 
 class Solution {
-    public class Process {
-        int num;
-        int value;
+    class task {
+        private int idx;
+        private int prior;
         
-        public Process(int num, int value) {
-            this.num = num;
-            this.value = value;
+        public task(int idx, int prior) {
+            this.idx = idx;
+            this.prior = prior;
         }
     }
+    
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        // 1. queue, pq 만들기
-        Queue<Process> queue = new LinkedList<>();
+        Queue<task> q = new LinkedList<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
-        // 2. 요소 넣기 
+        // 1. 요소 넣기
         for(int i = 0; i < priorities.length; i++) {
-            queue.offer( new Process(i + 1, priorities[i]) );
+            q.offer(new task(i, priorities[i]));
             pq.offer(priorities[i]);
         }
         
-        // 3. 조건 구현
-        while( !queue.isEmpty() ) {
-            Process process = queue.poll();
-            // 4. 만약 현재 우선순위보다 높은 프로세스가 있다면 
-            if( process.value < pq.peek() ) {
-                queue.offer(process);
+        while( !q.isEmpty() ) {
+            task curr = q.poll();
+            
+            // 2. 현재 요소보다 큰 게 있다면 
+            if( curr.prior < pq.peek() ) {
+                q.offer(curr);
             }
-            // 5. 현재 우선순위가 가장 높다면 
             else {
                 pq.poll();
-                answer ++;
                 
-                // 6. 문제에 해당하는 답 찾기
-                if( process.num == location + 1) {
-                    return answer;
-                }   
+                answer++;
+                if( location == curr.idx ) return answer;
             }
         }
+        
         return answer;
     }
-    
 }
