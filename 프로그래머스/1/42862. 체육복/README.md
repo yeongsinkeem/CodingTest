5976607c31f687322a8213b968f1777e2f9b691b
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-12-18 15:40:20 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
 
 ### 성능 요약
 
-메모리: 80.8 MB, 시간: 0.03 ms
+메모리: 73.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 23일 15:51:04
+2025년 12월 18일 15:40:18
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,45 +1,43 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
+        int[] students = new int[n];
         
-        // 1. 전체 배열 생성
-        int[] total = new int[n+1];
-        for(int i = 1; i <= n; i++) {
-            total[i] = 1;
+        for(int i = 0; i < students.length; i++) {
+            students[i] = 1;
         }
         
-        // 2. 잃어버렸다면 - 1
-        for(int l : lost) {
-            total[l]--;
+        // 도난
+        for(int i = 0; i < lost.length; i++) {
+            int num = lost[i];
+            students[num - 1] = 0;
         }
         
-        // 3. 여분 있다면 + 1
-        for(int r : reserve) {
-            total[r]++;
+        // 여분
+        for(int i = 0; i < reserve.length; i++) {
+            int num = reserve[i];
+            students[num - 1] = students[num - 1] + 1;
         }
         
-        // 4. 대여
-        for(int i = 1; i <= n; i++) {
-            if( total[i] == 0) {
-                // 앞 번호 학생에게 빌릴 수 있는지
-                if( i > 1 && total[i-1] == 2 ) {
-                    total[i] = 1;
-                    total[i-1] = 1;
+        for(int i = 0; i < n; i++) {
+            if( students[i] == 0 ) {
+                // 이전 번호한테 빌릴 수 있는 경우 
+                if( i - 1 >= 0 && students[i-1] == 2 ) {
+                    students[i-1] = 1;
+                    students[i] = 1;
                 }
-                // 뒷 번호 학생에게 빌릴 수 있는지
-                else if( i + 1 <= n && total[i+1] == 2 ) {
-                    total[i+1] = 1;
-                    total[i] = 1;
+                
+                // 다음 번호한테 빌릴 수 있는 경우
+                else if( i + 1 < n && students[i+1] == 2 ) {
+                    students[i+1] = 1;
+                    students[i] = 1;
                 }
             }
         }
         
-        for(int i = 0; i < total.length; i++) {
-            if( total[i] >= 1 ) answer++;
+        for(int i = 0; i < n; i++) {
+            if( students[i] >= 1 ) answer++;
         }
-        
         return answer;
     }
 }
