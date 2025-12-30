6d6315c05f0bbf9e46724a06d59699e295e710c3
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2025-12-31 00:01:33 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 124 ms, Memory: 14920 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -1,37 +1,37 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        // 문제에서 주어지는 테스트 케이스 수 (필요 없다면 N만 입력받아도 됨)
-        int T = sc.nextInt();
-        
-        // 최대 범위가 10,000이라고 가정할 때의 DP 배열
-        int[] dp = new int[10001];
-
-        // 초기값: 0을 만드는 방법은 아무것도 선택하지 않는 1가지로 설정
-        dp[0] = 1;
-
-        // 1. 숫자 1만 사용하여 만드는 경우의 수
-        for (int i = 1; i <= 10000; i++) {
-            dp[i] += dp[i - 1];
-        }
-
-        // 2. 숫자 2를 추가로 사용하여 만드는 경우의 수
-        for (int i = 2; i <= 10000; i++) {
-            dp[i] += dp[i - 2];
-        }
-
-        // 3. 숫자 3을 추가로 사용하여 만드는 경우의 수
-        for (int i = 3; i <= 10000; i++) {
-            dp[i] += dp[i - 3];
-        }
-
-        // 결과 출력
-        for (int t = 0; t < T; t++) {
-            int n = sc.nextInt();
-            System.out.println(dp[n]);
-        }
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] answer = new int[N];
+		
+		// 1. 점화식 
+		int[][] dp = new int[10001][4];
+		dp[0][1] = 1;
+		dp[0][2] = 0;
+		dp[0][3] = 0;
+		
+		for(int i = 1; i <= 10000; i++) {
+			if( i - 1 >= 0 ) dp[i][1] = dp[i-1][1];
+			if( i - 2 >= 0 ) dp[i][2] = dp[i-2][1] + dp[i-2][2];
+			if( i - 3 >= 0 ) dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+		}
+		
+		// 2. 숫자 입력받기 
+		for(int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			answer[i] = dp[num][1] + dp[num][2] + dp[num][3];
+		}
+		
+		for(int i : answer) {
+			System.out.println(i);
+		}
+		
+	}
 }
