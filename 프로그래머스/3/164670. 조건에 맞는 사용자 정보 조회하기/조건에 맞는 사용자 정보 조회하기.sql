f48abfe8091599eee875ccb969665bfd6fc99d1a
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2026-01-02 12:39:54 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,25 @@
+-- 코드를 입력하세요
+-- 중고 거래 게시물을 3건 이상 등록한 사용자 
+/*
+SELECT WRITER_ID, COUNT(*) AS COUNT
+FROM USED_GOODS_BOARD
+GROUP BY WRITER_ID
+HAVING COUNT(*) >= 3;
+*/
+
+SELECT A.USER_ID, 
+       A.NICKNAME,
+       CONCAT(A.CITY, ' ', A.STREET_ADDRESS1, ' ', A.STREET_ADDRESS2) AS '전체주소', 
+       CONCAT(
+             SUBSTRING(A.TLNO, 1, 3), '-',
+             SUBSTRING(A.TLNO, 4, 4), '-',
+             SUBSTRING(A.TLNO, 8, 4) ) AS '전화번호'
+FROM USED_GOODS_USER A
+JOIN(
+    SELECT WRITER_ID, COUNT(*) AS COUNT
+    FROM USED_GOODS_BOARD
+    GROUP BY WRITER_ID
+    HAVING COUNT(*) >= 3
+) S
+ON S.WRITER_ID = A.USER_ID
+ORDER BY A.USER_ID DESC;
