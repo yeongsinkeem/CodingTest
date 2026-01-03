7c36cce7a6fe3e68367cecddb4c6065a9e0a4005
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2026-01-04 00:09:21 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 84.6 MB, 시간: 9.45 ms
+메모리: 74.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 28일 18:44:07
+2026년 01월 04일 00:09:19
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,22 +2,22 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        // 1. 전체 개수 입력 
         int total = brown + yellow;
         
-        // 2. 공약수 구하기
-        for(int i = 2; i <= total ; i++) {
+        // 1. 약수 순회 
+        for(int i = 1; i <= total; i++) {
             if( total % i == 0 ) {
                 int a = i;
                 int b = total / i;
                 
-                if( (a - 2) * (b - 2) == yellow ) {
-                    answer[0] = a;
-                    answer[1] = b;
+                if( (a * 2) + (b * 2) - 4 == brown ) {
+                    answer[0] = b;
+                    answer[1] = a;
+                    
+                    return answer;
                 }
             }
         }
-        
         return answer;
     }
 }
