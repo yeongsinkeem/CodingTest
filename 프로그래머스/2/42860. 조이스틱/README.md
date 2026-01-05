d107224e9fe71e96e124b34114638455d43e525e
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2026-01-06 00:57:44 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 77.3 MB, 시간: 0.08 ms
+메모리: 89.2 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 16일 11:32:12
+2026년 01월 06일 00:57:42
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,32 +1,30 @@
 class Solution {
     public int solution(String name) {
+        int answer = 0;
         char[] c = name.toCharArray();
-        int alpha = 0;
         
-        // 1. 알파벳 계산
+        // 1. 알파벳 
+        int alpha = 0;
         for(int i = 0; i < c.length; i++) {
             int diff = c[i] - 'A';
             alpha += Math.min(diff, 26 - diff);
         }
         
-        // 2. 거리 계산
-        // 2-1. 쭉 가기
-        int move = name.length() - 1;
-        
+        // 2. 거리
+        int move = c.length - 1;
         for(int i = 0; i < c.length; i++) {
             int next = i + 1;
-            
-            // 연속된 A 건너뛰기
-            while( next < c.length && c[next] == 'A') {
+            // 연속된 A들 찾기 
+            while( next < c.length && c[next] == 'A' ) {
                 next++;
             }
-            // 2-2. 순 -> 역
-            move = Math.min(move, i + i + (c.length - next));
             
-            // 2-3. 역 -> 순
-            move = Math.min(move, (c.length - next) * 2 + i );
+            // 순방향 -> 역방향 
+            move = Math.min( move, i + i + (c.length - next) );
+            // 역방향 -> 순방향
+            move = Math.min( move, (c.length - next) * 2 + i );
         }
         
-        return alpha + move;
+        return move + alpha;
     }
 }
