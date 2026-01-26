a607fd6db78fa16798b8c8f67a03f05c0f644503
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2026-01-26 21:30:14 +0900

    [Silver V] Title: 돌 게임, Time: 108 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,19 +1,40 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        // 돌의 개수 N 입력
-        int N = sc.nextInt();
-        
-        // N이 홀수이면 상근(SK), 짝수이면 창영(CY)
-        if (N % 2 == 1) {
-            System.out.println("SK");
-        } else {
-            System.out.println("CY");
-        }
-        
-        sc.close();
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		String[] arr = new String[2];
+		arr[0] = "SK";
+		arr[1] = "CY";
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int a = N / 3;
+		int b = N % 3;
+		
+		// 짝수
+		// 나머지가 1 -> 상근 
+		if( a % 2 == 0 ) {
+			if( b == 1 ) {
+				System.out.println("SK");
+			}
+			else {
+				System.out.println("CY");
+			}
+		}
+		
+		// 홀수 
+		else {
+			if( b == 1 ) {
+				System.out.println("CY");
+			}
+			else {
+				System.out.println("SK");
+			}
+		}
+		
+	}
+	
 }
