95c29e7530f49810c48a33a266817d0affee447b
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2026-01-26 15:44:40 +0900

    [Silver V] Title: 줄세우기, Time: 212 ms, Memory: 17284 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -6,34 +6,33 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int M = Integer.parseInt(br.readLine());
 		
-		for(int i = 0; i < M; i++) {
+		int P = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < P; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int num = Integer.parseInt(st.nextToken());
 			
-			int[] students = new int[20];
-			int total = 0;
+			int T = Integer.parseInt(st.nextToken());
 			
+			// 1. 배열 설정 
+			int[] arr = new int[20];
 			for(int j = 0; j < 20; j++) {
 				int curr = Integer.parseInt(st.nextToken());
-				
-				int countLarger = 0;
-				
-				// 이미 줄 서있는 학생들과 비교 
-				for(int k = 0; k < j; k++) {
-					// 이동해야 함 
-					if( students[k] > curr ) {
-						countLarger++;
-					}
-				}
-				
-				total += countLarger;
-				// 현재 학생은 인덱스 그대로 삽입(실제 위치 이동 필요 x. 어차피 고정돼있어서) 
-				students[j] = curr;
+				arr[j] = curr;
 			}
 			
-			System.out.println(num + " " + total );
+			// 2. 정렬 
+			int sum = 0;
+			for(int j = 0; j < 20; j++) {
+				// 나보다 큰 애들만큼 더하기 
+				for(int k = 0; k < j; k++) {
+					if(arr[k] > arr[j]) {
+						sum ++;
+					}
+				}
+			}
+			
+			System.out.println( T + " " + sum);
 		}
 	}
 
