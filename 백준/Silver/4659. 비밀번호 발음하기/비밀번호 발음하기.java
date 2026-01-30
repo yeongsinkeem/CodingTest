4a2158f77290a63dc251e790929103c8acfa5739
yeonsinkeem <144760119+yeongsinkeem@users.noreply.github.com>
2026-01-30 21:58:54 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 108 ms, Memory: 15804 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -5,14 +5,15 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		String word = "";
 		
 		while( true ) {
 			word = br.readLine();
 			
-			if (word.equals("end")) break;
-			
-			// 1-1. 모음 포함 여부 확인 
+            if( word.equals("end") ) break;
+            
+			// 1. 모음 포함
 			boolean flag = false;
 			for(char c : word.toCharArray()) {
 				if( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ) {
@@ -20,13 +21,14 @@ public class Main {
 					break;
 				}
 			}
-			// 1-2. 만약 모음 없다면 바로 종료 
+			
+			// 1-2. 만약 모음이 없다면 종료 
 			if( !flag ) {
 				System.out.println("<" + word + "> is not acceptable.");
 				continue;
 			}
 			
-			// 2. 3개 연속 자음/모음 X
+			// 2. 3연속 자음 모음 X
 			if( isThree(word) ) {
 				System.out.println("<" + word + "> is not acceptable.");
 				continue;
@@ -39,6 +41,7 @@ public class Main {
 			}
 			
 			System.out.println("<" + word + "> is acceptable.");
+			
 		}
 	}
 	
@@ -46,9 +49,8 @@ public class Main {
 		for(int i = 1; i < s.length() - 1; i++) {
 			char a = s.charAt(i-1);
 			char b = s.charAt(i);
-			char c = s.charAt(i+1);
+			char c = s.charAt(i + 1);
 			
-			// 모두 모음 
 			if( a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' ) {
 				if( b == 'a' || b == 'e' || b == 'i' || b == 'o' || b == 'u' ) {
 					if( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ) {
@@ -56,7 +58,6 @@ public class Main {
 					}
 				}
 			}
-			// a가 자음일 때 -> b, c도 자음이면 종료 
 			else {
 				if( b != 'a' && b != 'e' && b != 'i' && b != 'o' && b != 'u' ) {
 					if( c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' ) {
@@ -65,21 +66,19 @@ public class Main {
 				}
 			}
 		}
-		
 		return false;
 	}
 	
 	public static boolean isSameTwo(String s) {
 		for(int i = 0; i < s.length() - 1; i++) {
 			char a = s.charAt(i);
-			char b = s.charAt(i + 1);
+			char b = s.charAt(i+1);
 			
 			if( a == b ) {
 				if( a == 'e' || a == 'o' ) continue;
 				return true;
 			}
 		}
-		
 		return false;
 	}
 }
